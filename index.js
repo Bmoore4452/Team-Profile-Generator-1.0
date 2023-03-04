1e74f1060e7c5937be0b0f6de7894bd7d6667a2b
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-03-04 16:04:29 -0500

    created Employee class skeleton

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const manager = require("./lib/Manager");
 
 const generateHtml = ({ manName, employeeId, emailOne, officeNumber }) =>
   `<!DOCTYPE html>
@@ -83,40 +84,125 @@ const generateHtml = ({ manName, employeeId, emailOne, officeNumber }) =>
 
 </html>`;
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "manName",
-      message: "What is the team manager's name?",
-    },
-    {
-      type: "input",
-      name: "employeeId",
-      message: "What is your ID number?",
-    },
-    {
-      type: "input",
-      name: "emailOne",
-      message: "What is your email address?",
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "What is your office number?",
-    },
-    {
-      type: "list",
-      name: "teamList",
-      message: "Select the members of your team:",
-      choices: ["Engineer","Intern"],
-      
-    },
-  ])
-  .then((answers) => {
-    const htmlPageContent = generateHtml(answers);
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       name: "manName",
+//       message: "What is the team manager's name?",
+//     },
+//     {
+//       type: "input",
+//       name: "employeeId",
+//       message: "What is your ID number?",
+//     },
+//     {
+//       type: "input",
+//       name: "emailOne",
+//       message: "What is your email address?",
+//     },
+//     {
+//       type: "input",
+//       name: "officeNumber",
+//       message: "What is your office number?",
+//     },
+//     {
+//       type: "list",
+//       name: "teamList",
+//       message: "Select the members of your team:",
+//       choices: ["Engineer","Intern"],
 
-    fs.writeFile("./dist/index.html", htmlPageContent, (err) =>
-      err ? console.log(err) : console.log("Successfully created index.html!")
-    );
-  });
+//     },
+//   ])
+//   .then((answers) => {
+//     const htmlPageContent = generateHtml(answers);
+
+//     fs.writeFile("./dist/index.html", htmlPageContent, (err) =>
+//       err ? console.log(err) : console.log("Successfully created index.html!")
+//     );
+//   });
+
+
+
+const engineer = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engName",
+        message: "What is the team manager's name?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is your ID number?",
+      },
+      {
+        type: "input",
+        name: "emailTwo",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your Github User name?",
+      },
+    ])
+    .then((answer) => {});
+};
+const employee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "teamList",
+        message: "Select the new member of your team:",
+        choices: ["Engineer", "Intern"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.teamList === "Engineer") {
+        engineer();
+      }
+    });
+};
+
+const manager = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "manName",
+        message: "What is the team manager's name?",
+      },
+      {
+        type: "input",
+        name: "employeeId",
+        message: "What is your ID number?",
+      },
+      {
+        type: "input",
+        name: "emailOne",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office number?",
+      },
+      {
+        type: "confirm",
+        name: "newMember",
+        message: "Do you want to add a new member?",
+      },
+    ])
+    .then((answer) => {
+      if (answer.newMember === true) {
+        employee();
+      }
+    });
+};
+
+
+
+manager();
